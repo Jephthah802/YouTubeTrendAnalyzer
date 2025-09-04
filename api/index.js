a83337d06895d0852003515681803afcb3a10509
Jephthah802 <jephewoh@gmail.com>
2025-09-04 16:55:07 +0100

    twenty second commit

--- api/index.js
+++ api/index.js
@@ -2,12 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import serverless from 'serverless-http';
-import connectDB from '../../src/config/db.js';
+import connectDB from '../src/config/db.js';
 
-import apiRoutes from '../../src/routes/api.js';
-import authRoutes from '../../src/routes/auth.routes.js';
-import favoritesRoutes from '../../src/routes/favorites.routes.js';
-import playlistRoutes from '../../src/routes/playlist.routes.js';
+import apiRoutes from '../src/routes/api.js';
+import authRoutes from '../src/routes/auth.routes.js';
+import favoritesRoutes from '../src/routes/favorites.routes.js';
+import playlistRoutes from '../src/routes/playlist.routes.js';
 
 dotenv.config();
 
