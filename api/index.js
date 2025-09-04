cf5d04a8fe66f352d55f043f91c4e216ba0347a2
Jephthah802 <jephewoh@gmail.com>
2025-09-04 17:07:54 +0100

    twenty three commit

--- api/index.js
+++ api/index.js
@@ -2,14 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import serverless from 'serverless-http';
-import connectDB from '../src/config/db.js';
+import connectDB from './src/config/db.js';
 
-import apiRoutes from '../src/routes/api.js';
-import authRoutes from '../src/routes/auth.routes.js';
-import favoritesRoutes from '../src/routes/favorites.routes.js';
-import playlistRoutes from '../src/routes/playlist.routes.js';
 
-dotenv.config();
+import apiRoutes from './src/routes/api.js'
+import authRoutes from './src/routes/auth.routes.js';
+import favoritesRoutes from './src/routes/favorites.routes.js';
+import playlistRoutes from './src/routes/playlist.routes.js';
+
+dotenv.connectDB();
 
 const app = express();
 
